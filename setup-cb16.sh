be18be60ddaf0faff6441b25ac54b2f580e6f9eb
Florian Eidner <florianeidner@googlemail.com>
2017-11-22 14:21:02 +0100

    Added setup script for agromicosbox

--- setup-cb16.sh
+++ setup-cb16.sh
@@ -15,12 +15,19 @@ echo "Make script executable"
 chmod 755 /etc/init.d/S54isoident
 
 echo "Create isoident folder"
-mkdir /media/disk/isodident
+mkdir /media/disk/isoident
 
 echo "Copy datasets to folder"
-cp -a datasets /media/disk/datasets
+cp -a datasets /media/disk/isoident/datasets
 
 echo "Copy webeditor files to folder"
-cp -a webeditor /media/disk/webeditor
+cp -a webeditor /media/disk/isoident/webeditor
 
-echo "Adjust /etc/nginx/nginx.conf"
+echo "########################################################################"
+echo "#                                      				     #"
+echo "# Please add the following line within http section in the nginx.conf: #"
+echo "#               (default: /etc/nginx/nginx.conf)                       #"
+echo "#                                      				     #"
+echo "#    include /media/disk/isoident/webeditor/nginx-isoident-lua.conf;   #"
+echo "#                                      				     #"
+echo "########################################################################"
